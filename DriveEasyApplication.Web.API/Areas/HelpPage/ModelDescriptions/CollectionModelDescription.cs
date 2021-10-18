12c893c91735dc641003aa6fe98ca130ad74caf2
snade <snade@LAPTOP-ANE1FVSL>
2021-10-18 10:43:02 +0530

    Added template for drive easy application

---
+++ DriveEasyApplication.Web.API/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace DriveEasyApplication.Web.API.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ DriveEasyApplication.Web.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DriveEasyApplication.Web.API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
