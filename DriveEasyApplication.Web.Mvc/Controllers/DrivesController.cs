27b262567d58b5eff4448932128cfb6d11b8c992
snade <snade@LAPTOP-ANE1FVSL>
2021-10-19 21:59:01 +0530

    Implemented the UI for application

---
+++ DriveEasyApplication.Web.Mvc/Controllers/DrivesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DriveEasyApplication.Web.Mvc.Controllers
+{
+    public class DrivesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
