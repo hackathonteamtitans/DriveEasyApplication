fca0cb4db6974a6ac44490c74bc1945e7968ed74
Suraj Patel <patelsurajmail@gmail.com>
2021-10-20 22:34:16 +0530

    services

---
+++ DriveEasyApplication.Web.Mvc/Services/IEasyDriveDbService.cs
@@ -0,0 +1,13 @@
+﻿using DriveEasyApplication.Web.Mvc.Models;
+using System.Collections.Generic;
+
+namespace DriveEasyApplication.Web.Mvc.Services
+{
+    interface IEasyDriveDbService
+    {
+        void Add<T>(T data);
+
+        void Add<T>(IList<T> data);
+
+    }
+}
