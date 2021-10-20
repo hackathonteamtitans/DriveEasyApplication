b8c2b27e1e06f4b3e6014ab41bfb8a4d169707c2
snade <snade@LAPTOP-ANE1FVSL>
2021-10-20 13:07:01 +0530

    Implemented candidate details page

--- DriveEasyApplication.Web.Mvc/Controllers/DrivesController.cs
+++ DriveEasyApplication.Web.Mvc/Controllers/DrivesController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace DriveEasyApplication.Web.Mvc.Controllers
@@ -11,6 +12,38 @@ namespace DriveEasyApplication.Web.Mvc.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult CandidateDetails()
+        {
+            return View();
+        }
+
+        private void hhh()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44356/api/");
+                //HTTP GET
+                var responseTask = client.GetAsync("Values");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    //var readTask = result.Content.ReadAsAsync<IList<StudentViewModel>>();
+                    //readTask.Wait();
+
+                    //students = readTask.Result;
+                }
+                else //web api sent error response 
+                {
+                    //log response status here..
+
+                    //students = Enumerable.Empty<StudentViewModel>();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
         }
     }
 }
