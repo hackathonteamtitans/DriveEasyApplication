d0fd9efdd56472ac0536dc6c3fa8c05b3ad46c8d
Abhishek Chorage <hackathonteamtitans@gmail.com>
2021-10-20 13:54:40 +0530

    Updated model data

--- DriveEasyApplication.Web.API/Models/Candidate.cs
+++ DriveEasyApplication.Web.API/Models/Candidate.cs
@@ -1,24 +1,44 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DriveEasyApplication.Web.API.Models
 {
     public class Candidate
     {
-        public string ID { get; set; }
+        public int? ID { get; set; }
         public string Name { get; set; }
-        public List<string> SkillSet { get; set; }
-        public string Source { get; set; }
-        public Show Show { get; set; }
-        public string Contact { get; set; }
         public string Email { get; set; }
-        public string Experience { get; set; }
-        public string NoticePeriod { get; set; }
-        public string InterviewTime { get; set; }
-        public string TechnicalPanel { get; set; }
+        public string MobileNumber { get; set; }
+        public List<string> Skills{ get; set; }
+        public Decimal? Experience { get; set; }
+        public int? NoticePeriod { get; set; }
+        public string Source { get; set; }
+        public Show? Confirmed { get; set; }
+        public string CurrentOrganization { get; set; }
+        public int? DriveID { get; set; }
         public string MeetingLink { get; set; }
-        public Status Status { get; set; }
-        public string Feedback { get; set; }
-        public string Note { get; set; }
+        public DateTime InterviewTime { get; set; }
+        public string TechnicalPanel { get; set; }
+        public Status? Status { get; set; }
+        public string FeedbackForm { get; set; }
         public string ResumeLink { get; set; }
+
+
+        public Dictionary<string,string>ToDictionary()
+        {
+            Dictionary<string, string> KeyValuePairs = new Dictionary<string, string>();
+            if (ID != null) KeyValuePairs.Add("ID", ID.ToString());
+            if (Skills != null) KeyValuePairs.Add("Skills", string.Join(",", Skills));
+            if (Experience != null) KeyValuePairs.Add("Experience", Experience.ToString());
+            if (NoticePeriod != null) KeyValuePairs.Add("NoticePeriod", NoticePeriod.ToString());
+            if (Confirmed != null) KeyValuePairs.Add("Confirmed", Confirmed.ToString());
+            if (DriveID != null) KeyValuePairs.Add("DriveID", DriveID.ToString());
+            if (InterviewTime != null) KeyValuePairs.Add("InterviewTime", InterviewTime.ToString());
+            if (Status != null) KeyValuePairs.Add("Status", Status.ToString());
+
+            return KeyValuePairs;
+        }
+
     }
 }
